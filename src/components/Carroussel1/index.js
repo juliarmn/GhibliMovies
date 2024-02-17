672df3699c89ce1e7ff6ff586e190a035dc29d7b
juliarmn <julia.rm.nascimento@gmail.com>
2024-02-16 21:55:23 -0300

    Comecando Seção Filmes

---
+++ src/components/Carroussel1/index.js
@@ -0,0 +1,11 @@
+import styles from './Carroussel1.module.css'
+
+function Carroussel1 () {
+    return (
+        <div className={styles.capa}
+        styles={{backgroundImage: `url('images/banner-${image}.png')'`}}>
+        </div>
+    )
+}
+
+export default Carroussel1;
--- src/components/Filmes/index.js
+++ src/components/Filmes/index.js
@@ -4,8 +4,10 @@ import styles from './Filmes.module.css'
 function Filmes() {
     return (
         <div className={styles.filmes}>
-            {}
-            <h1>Filmes</h1>
+            <div className={styles.titulo}>
+                <h1>FILMES</h1>
+            </div>
+            <div className={styles.barra}></div>
         </div>
     ) 
 } 
